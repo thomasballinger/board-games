32e48f6e41cc702564b6e5533ddeda7ae421f35e
Dan Puttick <dputtick@users.noreply.github.com>
2016-04-06 17:24:22 -0400

    added tests

--- connect4/connect4.py
+++ connect4/connect4.py
@@ -1,56 +1,72 @@
+def board_maker(x, y):
+	'''Returns a board data structure based on the two dimensions passed in'''
+	column = [' ' for y in range(y)]
+	board = [column for x in range(x)]
+	return board
 
 
-def board_size():
-	# either ask the user for input, two numbers, board size
-	# or just use two variables set before the script is run
-	# returns a data structure that contains the board
-	# what data structure makes sense here? table? which direction should the rows go?
-	pass
-
-def board_drawer(board):
-	# figure out some way of displaying the board that gives the player
-	# the information they need
-	pass
+def board_printer(board):
+	'''Takes the current board structure and prints it to the terminal'''
+	board_width = len(board)
+	board_height = len(board[0])
+	print('\n')
+	for num in range(board_height):
+		i = num + 1
+		row = [column[-i] for column in board]
+		print(row)
+	print('\n')
+	print([str(i + 1) for i in range(board_width)])
+			
 
 def move_getter(player, board):
-	# ask the player to input a move
-	# check that it is a valid move
-	# add the move to the board
-	# return the new board and the move that was made
-	pass
+	player_piece = {'P1': 'X', 'P2': 'O'}
+	move = input('{}, type the number of desired column:\n'.format(player))
+	move_int = int(move)
+	move_index = move_int - 1
+	while True:
+		if move in [str(i) for i in range(len(board))]:
+			if board[move_int][-1] == ' ':
+				insert_spot = list(reversed(board[move_int])).index(' ')
+				insert_index = -(insert_spot + 1)
+				board[move_int][insert_index] = player_piece[player]
+				return board, move
+			else:
+				move = input('That column is full! Pick another:\n')
+		else:
+			move = input('Please enter a number of a column:\n')
+	
 
 def victory_checker(board):
-	# compare the board against some set victory conditions
-	# return true or false and the victor depending on the result
 	pass
+	# '''Returns True if victory, False if continue, and 'draw' if a tie'''
 
 def draw_checker(board):
+	pass
 	# check if there are any valid moves remaining
 	# return True if there aren't any
-	pass
 
 def move_recorder(move, move_history):
+	pass
 	# take the last move and add it to the move history
 	# return the new move history
-	pass
+
 
 
 
 def main():
 	pass
 
-if __name__ == '__main__':
-	# main()
+# if __name__ == '__main__':
+# 	main()
 
 
 
 
 
-# Function that returns the desired board size
+'''Function that returns the desired board size
 # Function that draws the board for the user
 # Function that gets a move and returns it
 # Function that adds the move to the board
 # Function that checks for victory conditions
 # Function that checks for draw conditions
-# Function that records the sequence of moves that were made
-
+# Function that records the sequence of moves that were made'''
---
+++ connect4/test.py
@@ -0,0 +1,34 @@
+import connect4 as c
+import unittest as u
+
+class Tests(u.TestCase):
+
+	def test_board_maker(self):
+		pass
+
+
+if __name__ == '__main__':
+	board = [
+			['X', ' ', ' ', ' ', ' '], 
+			['X', ' ', ' ', ' ', ' '],
+			['X', 'X', ' ', ' ', ' '],
+			['X', ' ', ' ', ' ', ' '],
+			['X', ' ', ' ', ' ', ' ']
+			]
+	# u.main()
+	board, move = c.move_getter('P1', board)
+	print(move)
+	c.board_printer(board)
+
+
+# should print a normal board as a list of lists
+# board = c.board_maker(5, 5)
+# print(board)
+
+# should print a properly displayed 5x5 board with 'X' on bottom row
+# board2 = [['X', ' ', ' ', ' ', ' '], 
+# 			['X', ' ', ' ', ' ', ' '],
+# 			['X', 'X', ' ', ' ', ' '],
+# 			['X', ' ', ' ', ' ', ' '],
+# 			['X', ' ', ' ', ' ', ' ']]
+# c.board_printer(board2)
