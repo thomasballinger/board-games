99b18899dfdcb121eab503b1d7cbed97ee01dbb7
Dan Puttick <dputtick@users.noreply.github.com>
2016-04-06 11:55:41 -0400

    reorganizing

---
+++ connect4/connect4.py
@@ -0,0 +1,56 @@
+
+
+def board_size():
+	# either ask the user for input, two numbers, board size
+	# or just use two variables set before the script is run
+	# returns a data structure that contains the board
+	# what data structure makes sense here? table? which direction should the rows go?
+	pass
+
+def board_drawer(board):
+	# figure out some way of displaying the board that gives the player
+	# the information they need
+	pass
+
+def move_getter(player, board):
+	# ask the player to input a move
+	# check that it is a valid move
+	# add the move to the board
+	# return the new board and the move that was made
+	pass
+
+def victory_checker(board):
+	# compare the board against some set victory conditions
+	# return true or false and the victor depending on the result
+	pass
+
+def draw_checker(board):
+	# check if there are any valid moves remaining
+	# return True if there aren't any
+	pass
+
+def move_recorder(move, move_history):
+	# take the last move and add it to the move history
+	# return the new move history
+	pass
+
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	# main()
+
+
+
+
+
+# Function that returns the desired board size
+# Function that draws the board for the user
+# Function that gets a move and returns it
+# Function that adds the move to the board
+# Function that checks for victory conditions
+# Function that checks for draw conditions
+# Function that records the sequence of moves that were made
+
