e895f6ec07866cf149f809d63a88b8c77eafdd2e
Thomas Ballinger <thomasballinger@gmail.com>
2016-04-08 13:52:38 -0400

    tabs -> spaces

--- connect4/connect4.py
+++ connect4/connect4.py
@@ -1,63 +1,63 @@
 def board_maker(x, y):
-	'''Returns a board data structure based on the two dimensions passed in'''
-	column = [' ' for y in range(y)]
-	board = [column for x in range(x)]
-	return board
+    '''Returns a board data structure based on the two dimensions passed in'''
+    column = [' ' for y in range(y)]
+    board = [column for x in range(x)]
+    return board
 
 
 def board_printer(board):
-	'''Takes the current board structure and prints it to the terminal'''
-	board_width = len(board)
-	board_height = len(board[0])
-	print('\n')
-	for num in range(board_height):
-		i = num + 1
-		row = [column[-i] for column in board]
-		print(row)
-	print('\n')
-	print([str(i + 1) for i in range(board_width)])
-			
+    '''Takes the current board structure and prints it to the terminal'''
+    board_width = len(board)
+    board_height = len(board[0])
+    print('\n')
+    for num in range(board_height):
+        i = num + 1
+        row = [column[-i] for column in board]
+        print(row)
+    print('\n')
+    print([str(i + 1) for i in range(board_width)])
+            
 
 def move_getter(player, board):
-	player_piece = {'P1': 'X', 'P2': 'O'}
-	move = input('{}, type the number of desired column:\n'.format(player))
-	move_int = int(move)
-	move_index = move_int - 1
-	while True:
-		if move in [str(i) for i in range(len(board))]:
-			if board[move_int][-1] == ' ':
-				insert_spot = list(reversed(board[move_int])).index(' ')
-				insert_index = -(insert_spot + 1)
-				board[move_int][insert_index] = player_piece[player]
-				return board, move
-			else:
-				move = input('That column is full! Pick another:\n')
-		else:
-			move = input('Please enter a number of a column:\n')
-	
+    player_piece = {'P1': 'X', 'P2': 'O'}
+    move = input('{}, type the number of desired column:\n'.format(player))
+    move_int = int(move)
+    move_index = move_int - 1
+    while True:
+        if move in [str(i) for i in range(len(board))]:
+            if board[move_int][-1] == ' ':
+                insert_spot = list(reversed(board[move_int])).index(' ')
+                insert_index = -(insert_spot + 1)
+                board[move_int][insert_index] = player_piece[player]
+                return board, move
+            else:
+                move = input('That column is full! Pick another:\n')
+        else:
+            move = input('Please enter a number of a column:\n')
+    
 
 def victory_checker(board):
-	pass
-	# '''Returns True if victory, False if continue, and 'draw' if a tie'''
+    pass
+    # '''Returns True if victory, False if continue, and 'draw' if a tie'''
 
 def draw_checker(board):
-	pass
-	# check if there are any valid moves remaining
-	# return True if there aren't any
+    pass
+    # check if there are any valid moves remaining
+    # return True if there aren't any
 
 def move_recorder(move, move_history):
-	pass
-	# take the last move and add it to the move history
-	# return the new move history
+    pass
+    # take the last move and add it to the move history
+    # return the new move history
 
 
 
 
 def main():
-	pass
+    pass
 
 # if __name__ == '__main__':
-# 	main()
+#     main()
 
 
 
--- connect4/test.py
+++ connect4/test.py
@@ -3,22 +3,22 @@ import unittest as u
 
 class Tests(u.TestCase):
 
-	def test_board_maker(self):
-		pass
+    def test_board_maker(self):
+        pass
 
 
 if __name__ == '__main__':
-	board = [
-			['X', ' ', ' ', ' ', ' '], 
-			['X', ' ', ' ', ' ', ' '],
-			['X', 'X', ' ', ' ', ' '],
-			['X', ' ', ' ', ' ', ' '],
-			['X', ' ', ' ', ' ', ' ']
-			]
-	# u.main()
-	board, move = c.move_getter('P1', board)
-	print(move)
-	c.board_printer(board)
+    board = [
+            ['X', ' ', ' ', ' ', ' '], 
+            ['X', ' ', ' ', ' ', ' '],
+            ['X', 'X', ' ', ' ', ' '],
+            ['X', ' ', ' ', ' ', ' '],
+            ['X', ' ', ' ', ' ', ' ']
+            ]
+    # u.main()
+    board, move = c.move_getter('P1', board)
+    print(move)
+    c.board_printer(board)
 
 
 # should print a normal board as a list of lists
@@ -27,8 +27,8 @@ if __name__ == '__main__':
 
 # should print a properly displayed 5x5 board with 'X' on bottom row
 # board2 = [['X', ' ', ' ', ' ', ' '], 
-# 			['X', ' ', ' ', ' ', ' '],
-# 			['X', 'X', ' ', ' ', ' '],
-# 			['X', ' ', ' ', ' ', ' '],
-# 			['X', ' ', ' ', ' ', ' ']]
+#           ['X', ' ', ' ', ' ', ' '],
+#           ['X', 'X', ' ', ' ', ' '],
+#           ['X', ' ', ' ', ' ', ' '],
+#           ['X', ' ', ' ', ' ', ' ']]
 # c.board_printer(board2)
